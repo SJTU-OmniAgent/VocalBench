f1f4da9030a3900487ea82c8d0f572c459c9afa9
LiuHeyang0114 <65945703+LiuHeyang0114@users.noreply.github.com>
2026-01-15 19:05:58 +0800

    Update whisper_asr.py

--- utils/whisper_asr/whisper_asr.py
+++ utils/whisper_asr/whisper_asr.py
@@ -13,14 +13,9 @@ import json
 import os 
 import numpy as np
 import argparse
-import pdb
 import sys
 import librosa
 from shutil import copyfile
-
-# EVAL_SETS = ["single_round", "creativity", "instruction", "knowledge", "multi_round", "reasoning", "safety", "emotion", "robust"]
-# EVAL_MODELS = ["mini-omni", "mini-omni2", "llama-omni", "freeze-omni", "baichuan-omni", "minicpm-o", "vocalnet", "glm-4-voice", "qwen2.5-omni", "kimi-audio", "slam-omni"]
-
 
 def load_whisper():
     local_model_path = "../tools/whisper" 
@@ -53,19 +48,7 @@ def remove_sp(text, language):
         gt = re.sub(rf"\s+", r"", gt)
     return gt
 
-
-def get_wave_file(eval_set, eval_model, index, qid):
-  return f"{qid}.wav"
-  # if eval_model == 'llama-omni':
-  #   return f"{str(index)}_pred.wav"
-  # elif eval_model in ['mini-omni', 'mini-omni2','baichuan-omni', 'freeze-omni', 'glm-4-voice', 'minicpm-o', 'qwen2.5-omni', 'kimi-audio', 'slam-omni']:
-  #   return f"{qid}.wav"
-  # elif eval_model in ['vocalnet']:
-  #   return f"{index}.wav"
-  # else:
-  #   return None
-
-def whisper_asr(input_json, output_json, wav_dir, eval_set, eval_model):
+def whisper_asr(input_json, output_json, wav_dir):
     language = 'en'
     asr_pipe = load_whisper()
     english_normalizer = EnglishTextNormalizer()
@@ -73,30 +56,20 @@ def whisper_asr(input_json, output_json, wav_dir, eval_set, eval_model):
       data = [json.loads(line) for line in infile]
     
     instances = []
-    index = 0
-    for item in data:
-        # index = item['question_id'].split('-')[1]
-        qid = item['question_id']
-        wav_path = os.path.join(wav_dir, get_wave_file(eval_set, eval_model, index, qid))
+    for index, item in enumerate(data):
+        qid = item['Qid']
+        wav_path = os.path.join(wav_dir, f"{qid}.wav")
         if os.path.exists(wav_path):
           audio_input, sample_rate = sf.read(wav_path)
           sample = {"raw": audio_input, "sampling_rate": sample_rate}
           result = asr_pipe(sample)
-          asr_pred = english_normalizer(remove_sp(result["text"], language))
+          response_hyp = english_normalizer(remove_sp(result["text"], language))
           print(asr_pred)
           instances.append({
-            "question_id": item['question_id'],
-            "prediction": item['prediction'],
-            "asr_pred": asr_pred
+            "Qid": item['Qid'],
+            "Response": item['Response'],
+            "Response_hyp": response_hyp
           })
-        else:
-          instances.append({
-            "question_id": item['question_id'],
-            "prediction": item['prediction'],
-            "asr_pred": ""
-          })
-        index += 1
-    
     with open(output_json, 'w+', encoding='utf-8') as f:
       json.dump(instances, f, ensure_ascii=False, indent=4)
 
@@ -105,9 +78,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_json", type=str)
     parser.add_argument("--output_json", type=str)
-    parser.add_argument("--wav_dir", type=str)
+    parser.add_argument("--audio_dir", type=str)
     args = parser.parse_args()
-    
-    eval_set = args.input_json.split('/')[-1].split('.')[0]
-    eval_model = args.input_json.split('/')[-3]
-    whisper_asr(args.input_json, args.output_json, args.wav_dir, eval_set, eval_model)
+    whisper_asr(args.input_json, args.output_json, args.audio_dir)
